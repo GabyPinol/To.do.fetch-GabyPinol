56741b3cfe05ea4eab0d4c0d90ef688a6bb26183
GabyPinol <gabypinol435@gmail.com>
2026-04-01 14:22:34 +0000

    Fetch agregado B)

--- src/js/components/ToDoApp.jsx
+++ src/js/components/ToDoApp.jsx
@@ -1,10 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 
 const ToDoApp = () => {
 	const [inputValue, setInputValue] = useState("");
 	const [list, setList] = useState([]);
+
+	const loadTodos = async () => {
+		const resp = await fetch("https://playground.4geeks.com/todo/users/Gaby", { method: "GET" })
+
+		if (resp.status === 404) {
+			// ejecutar func de crear usuario con fetch
+			console.log("hay que crear el user")
+		} else {
+			const data = await resp.json()
+
+			setList(data.todos);
+		}
+
+	}
+
+	const createTodo = async () => {
+		const resp = await fetch("https://playground.4geeks.com/todo/todos/Gaby", {
+			method: "POST",
+			headers: {
+				"Content-Type":"application/json"
+			},
+			body: JSON.stringify({
+				"label": inputValue,
+				"is_done": false
+			})
+		})
+		resp.status === 201 && loadTodos()
+	}
+
+	const deleteTodo = async (id) => {
+		const resp = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+			method: "DELETE"
+		})
+		resp.status === 204 && loadTodos()
+	}
+
+	useEffect(() => {
+		loadTodos()
+	}, [])
 
 	return (
 		<div className="container mt-5">
@@ -19,7 +58,7 @@ const ToDoApp = () => {
 					onKeyDown={(e) => {
 						if (e.key === "Enter") {
 							if (inputValue.trim() !== "") {
-								setList([...list, inputValue]);
+								createTodo()
 								setInputValue("");
 							}
 						}
@@ -30,16 +69,16 @@ const ToDoApp = () => {
 					<ul className="list-group list-group-flush">
 						{list.map((item, index) => (
 							<li key={index} className="list-group-item d-flex justify-content-between align-items-center p-3 fs-5 task-item">
-								{item}
+								{item.label}
 								<i
 									className="fas fa-times text-danger delete-icon"
-									onClick={() => setList(list.filter((t, i) => i !== index))}
+									onClick={() => deleteTodo(item.id)}
 								></i>
 							</li>
 						))}
 
 						<li className="list-group-item text-secondary p-2" style={{ fontSize: "12px" }}>
-							{list.length} {list.length === 1 ? "tarea pendiente" : "tareas pendientes"} 
+							{list.length} {list.length === 1 ? "tarea pendiente" : "tareas pendientes"}
 						</li>
 					</ul>
 				</ul>
